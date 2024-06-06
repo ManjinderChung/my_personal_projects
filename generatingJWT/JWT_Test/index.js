d92d99fd1caf0a7a87b5944941bc46f24d532fd8
ManjinderChung <162899350+ManjinderChung@users.noreply.github.com>
2024-06-06 20:54:22 +0100

    Add files via upload

---
+++ generatingJWT/JWT_Test/index.js
@@ -0,0 +1,58 @@
+const jwt = require("jsonwebtoken");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 8000;
+// Allows us to parse the body of a request
+app.use(bodyParser.json());
+// User login
+app.post("/login", (req, res) => {
+  // Req.body is sent by the client
+  const usr = req.body.username;
+  const pwd = req.body.password;
+  if (usr === "zama" && pwd === "abcdef") {
+    payload = {
+      name: usr,
+      admin: false,
+    };
+    const token = jwt.sign(JSON.stringify(payload), "jwt-secret", {
+      algorithm: "HS256",
+    });
+    res.send({ token: token });
+
+  } else {
+    res.status(403).send({ err: "Incorrect login!" });
+  }
+});
+// Start the server
+app.listen(PORT, () =>
+  console.log(`Now listening at http://localhost:${PORT}`)
+);
+app.get("/resource", (req, res) => {
+  const auth = req.headers["authorization"];
+  const token = auth.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, "jwt-secret");
+    res.send({
+      msg: `Hello, ${decoded.name}! Your JSON Web Token has been verified.`,
+    });
+  } catch (err) {
+    res.status(401).send({ err: "Bad JWT!" });
+  }
+});
+app.get("/admin_resource", (req, res) => {
+  const token = req.headers["authorization"].split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, "jwt-secret");
+    if (decoded.admin) {
+      res.send({ msg: "Success!" });
+    } else {
+      res
+        .status(403)
+        .send({ msg: "Your JWT was verified, but you are not an admin." });
+    }
+  } catch (e) {
+    res.sendStatus(401);
+  }
+});
